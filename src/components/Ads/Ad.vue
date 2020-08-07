fb56c877abaef13c1e81d16146b615b0949b5235
12tanais <leszek.koba@termagroup.pl>
2020-08-07 16:53:21 +0200

    new project

---
+++ src/components/Ads/Ad.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "Ad"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Ads/NewAd.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "NewAd"
+    }
+</script>
+
+<style scoped>
+
+</style>
